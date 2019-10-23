05d6cb3cb34df3a4e96dc042f67897aac72c1f3b
Andrey Zentsov <48022793+Andrew-Welens@users.noreply.github.com>
2019-10-23 17:47:17 +0300

    Update main.js

--- Dyslexia/main.js
+++ Dyslexia/main.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    function node(node) {
-        const textNodes = [], nonSpace = /\S/;
+    function getNodes(node) {
+        const textNodes = [], space = /\S/;
 
         function getText(node) {
             if (node.nodeType === 3) {
-                if (nonSpace.test(node.nodeValue)) {
+                if (space.test(node.nodeValue)) {
                     textNodes.push(node);
                 }
             } else {
@@ -18,10 +18,10 @@ document.addEventListener('DOMContentLoaded', () => {
         return textNodes;
     }
 
-    const textNodes = node(document.querySelector('.content'));
-    const wordsInTextNodes = [];
-    for (let i = 0; i < textNodes.length; i++) {
-        const node = textNodes[i];
+    const text = getNodes(document.querySelector('.content'));
+    const textNodes = [];
+    for (let i = 0; i < text.length; i++) {
+        const node = text[i];
         const words = [];
         const re = /[a-zA-Zа-яА-Я]+/g;
         let match;
@@ -33,33 +33,33 @@ document.addEventListener('DOMContentLoaded', () => {
                 position: position
             });
         }
-        wordsInTextNodes[i] = words;
+        textNodes[i] = words;
     }
 
-    function messUpWords() {
-        for (let i = 0; i < textNodes.length; i++) {
-            const node = textNodes[i];
-            for (let j = 0; j < wordsInTextNodes[i].length; j++) {
+    function upWords() {
+        for (let i = 0; i < text.length; i++) {
+            const node = text[i];
+            for (let j = 0; j < textNodes[i].length; j++) {
                 if (Math.random() > 1 / 10) {
                     continue;
                 }
-                const wordMeta = wordsInTextNodes[i][j];
+                const wordMeta = textNodes[i][j];
                 const word = node.nodeValue.slice(wordMeta.position, wordMeta.position + wordMeta.length);
                 const before = node.nodeValue.slice(0, wordMeta.position);
                 const after = node.nodeValue.slice(wordMeta.position + wordMeta.length);
-                node.nodeValue = before + messUpWord(word) + after;
+                node.nodeValue = before + upWord(word) + after;
             }
         }
     }
 
-    function messUpWord(word) {
+    function upWord(word) {
         if (word.length < 3) {
             return word;
         }
-        return word[0] + messUpMessyPart(word.slice(1, -1)) + word[word.length - 1];
+        return word[0] + UpMessy(word.slice(1, -1)) + word[word.length - 1];
     }
 
-    function messUpMessyPart(messyPart) {
+    function UpMessy(messyPart) {
 
         if (messyPart.length < 2) {
             return messyPart;
@@ -78,6 +78,5 @@ document.addEventListener('DOMContentLoaded', () => {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
-    setInterval(messUpWords, 50);
+    setInterval(upWords, 50);
 });
-
