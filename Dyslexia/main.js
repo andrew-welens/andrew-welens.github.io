6caec064463f234e83031e4817cbebdb1f19e5d9
Andrey Zentsov <48022793+Andrew-Welens@users.noreply.github.com>
2020-02-19 10:30:36 +0300

    Update main.js

--- Dyslexia/main.js
+++ Dyslexia/main.js
@@ -44,12 +44,10 @@ document.addEventListener('DOMContentLoaded', () => {
                     continue;
                 }
                 const wordMeta = wordsInTextNodes[i][j],
-                wordMeta = textNodes[i][j],
                 word = node.nodeValue.slice(wordMeta.position, wordMeta.position + wordMeta.length),
                 before = node.nodeValue.slice(0, wordMeta.position),
                 after = node.nodeValue.slice(wordMeta.position + wordMeta.length);
                 node.nodeValue = before + messUpWord(word) + after;
-                node.nodeValue = before + upWord(word) + after;
             }
         }
     }
